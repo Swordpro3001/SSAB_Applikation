47ebbe277a8683b5147c8212a92e60692d507538
Nico Furtner <nfurtner@student.tgm.ac.at>
2024-05-03 09:02:35 +0200

    Pause Fortfahren mit Funktion

--- app/src/main/java/com/example/myapplication/StartPage_Page2.java
+++ app/src/main/java/com/example/myapplication/StartPage_Page2.java
@@ -5,13 +5,16 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class StartPage_Page2 extends AppCompatActivity {
 
-    private Button button1, button2, button3;
+    private Button button1, button2, fortfahrenpause;
+    private ImageView pause, fortfahren;
     private boolean isExpanded = false;
     private Animation slideUpAnimation, slideDownAnimation;
 
@@ -22,15 +25,27 @@ public class StartPage_Page2 extends AppCompatActivity {
 
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
+        fortfahrenpause = findViewById(R.id.fortfahrenpause);
+
+        pause = findViewById(R.id.pause);
+        fortfahren = findViewById(R.id.fortfahren);
 
         slideUpAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
         slideDownAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+
+        pause.setVisibility(View.GONE);
+        fortfahren.setVisibility(View.GONE);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toggle();
+            }
+        });
+        fortfahrenpause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleImageViews();
             }
         });
     }
@@ -40,13 +55,25 @@ public class StartPage_Page2 extends AppCompatActivity {
             button1.setText("Start");
             button1.startAnimation(slideDownAnimation);
             button2.setVisibility(View.GONE);
-            button3.setVisibility(View.GONE);
+            fortfahrenpause.setVisibility(View.GONE);
+            pause.setVisibility(View.GONE);
+            fortfahren.setVisibility(View.GONE);
+
         } else {
             button1.setText("Stop");
             button1.startAnimation(slideUpAnimation);
             button2.setVisibility(View.VISIBLE);
-            button3.setVisibility(View.VISIBLE);
+            fortfahrenpause.setVisibility(View.VISIBLE);
+            pause.setVisibility(View.VISIBLE);
         }
         isExpanded = !isExpanded;
+    }
+    private void toggleImageViews() {
+        // Umkehrung der Sichtbarkeit von pause und fortfahren
+        int pauseVisibility = pause.getVisibility();
+        int fortfahrenVisibility = fortfahren.getVisibility();
+
+        pause.setVisibility(fortfahrenVisibility);
+        fortfahren.setVisibility(pauseVisibility);
     }
 }
